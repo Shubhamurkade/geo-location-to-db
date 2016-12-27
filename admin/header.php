a6974ebce6d4273ec9c5b41e621206bde68b0027
Shubhamurkade <shubhamurkade1@gmail.com>
2016-12-27 19:41:35 +0530

    Create header.php

---
+++ admin/header.php
@@ -0,0 +1,21 @@
+<header>
+			<nav>
+				
+					<li class="no-padding">
+						<ul class="collapsible collapsible-accordion">
+							<li>
+								<a class="collapsible-header waves-effect waves-teal">Location</a>
+								<div class="collapsible-body">
+									<ul>
+										<li><a href="add-location.php">New</a></li>
+										<li><a href="modify-location.php">Modify</a></li>
+										<li><a href="delete-location.php">Delete</a></li>
+									</ul>
+								</div>
+							</li>		  
+						</ul>
+					</li>		  
+					
+				</ul>
+			</nav>
+		</header>
